30e10b782289be1f0ca10cc43f1952b297655bfc
johnhwang20221847 <135480542+johnhwang20221847@users.noreply.github.com>
2026-06-02 20:26:06 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
+  apiKey: "AIzaSyAxjdIZc5eDEgzo86SWLOrXCAwoQHyETa8",
   authDomain: "elder-food-safety.firebaseapp.com",
   projectId: "elder-food-safety",
   storageBucket: "elder-food-safety.firebasestorage.app",
   messagingSenderId: "929697381233",
-  appId: "1:929697381233:web:fbc..."
+  appId: "1:929697381233:web:fbcb7a36a86fe166a454d0",
+  measurementId: "G-1W7BBHZG4C"
 };
 
 const app = initializeApp(firebaseConfig);
